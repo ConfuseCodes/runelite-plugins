c717e5cd8735cf506ac783a1691eec9a08309959
ConfuseCodes <nexusconfuse@gmail.com>
2025-09-18 13:12:23 -0400

    Update HttpServerPlugin.java

--- src/main/java/info/sigterm/plugins/httpserver/HttpServerPlugin.java
+++ src/main/java/info/sigterm/plugins/httpserver/HttpServerPlugin.java
@@ -7,6 +7,7 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
@@ -24,113 +25,129 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.http.api.RuneLiteAPI;
 
 @PluginDescriptor(
-	name = "HTTP Server"
+    name = "HTTP Server"
 )
 public class HttpServerPlugin extends Plugin
 {
-	@Inject
-	private Client client;
+    @Inject
+    private Client client;
 
-	@Inject
-	private ClientThread clientThread;
+    @Inject
+    private ClientThread clientThread;
 
-	private HttpServer server;
+    @Inject
+    private HttpServerConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		server = HttpServer.create(new InetSocketAddress(8080), 0);
-		server.createContext("/stats", this::handleStats);
-		server.createContext("/inv", handlerForInv(InventoryID.INVENTORY));
-		server.createContext("/equip", handlerForInv(InventoryID.EQUIPMENT));
-		server.setExecutor(Executors.newSingleThreadExecutor());
-		server.start();
-	}
+    private HttpServer server;
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		server.stop(1);
-	}
+    @Override
+    protected void startUp() throws Exception
+    {
+        int port = config.port();
 
-	public void handleStats(HttpExchange exchange) throws IOException
-	{
-		JsonArray skills = new JsonArray();
-		for (Skill skill : Skill.values())
-		{
-			if (skill == Skill.OVERALL)
-			{
-				continue;
-			}
+        // Try binding, auto-increment if in use
+        while (true)
+        {
+            try
+            {
+                server = HttpServer.create(new InetSocketAddress(port), 0);
+                break; // success
+            }
+            catch (BindException e)
+            {
+                port++;
+            }
+        }
 
-			JsonObject object = new JsonObject();
-			object.addProperty("stat", skill.getName());
-			object.addProperty("level", client.getRealSkillLevel(skill));
-			object.addProperty("boostedLevel", client.getBoostedSkillLevel(skill));
-			object.addProperty("xp", client.getSkillExperience(skill));
-			skills.add(object);
-		}
+        server.createContext("/stats", this::handleStats);
+        server.createContext("/inv", handlerForInv(InventoryID.INVENTORY));
+        server.createContext("/equip", handlerForInv(InventoryID.EQUIPMENT));
+        server.setExecutor(Executors.newSingleThreadExecutor());
+        server.start();
 
-		exchange.sendResponseHeaders(200, 0);
-		try (OutputStreamWriter out = new OutputStreamWriter(exchange.getResponseBody()))
-		{
-			RuneLiteAPI.GSON.toJson(skills, out);
-		}
-	}
+        System.out.println("HTTP Server started on port: " + port);
+    }
 
-	private HttpHandler handlerForInv(InventoryID inventoryID)
-	{
-		return exchange -> {
-			Item[] items = invokeAndWait(() -> {
-				ItemContainer itemContainer = client.getItemContainer(inventoryID);
-				if (itemContainer != null)
-				{
-					return itemContainer.getItems();
-				}
-				return null;
-			});
+    @Override
+    protected void shutDown() throws Exception
+    {
+        if (server != null)
+        {
+            server.stop(1);
+        }
+    }
 
-			if (items == null)
-			{
-				exchange.sendResponseHeaders(204, 0);
-				return;
-			}
+    public void handleStats(HttpExchange exchange) throws IOException
+    {
+        JsonArray skills = new JsonArray();
+        for (Skill skill : Skill.values())
+        {
+            if (skill == Skill.OVERALL) continue;
 
-			exchange.sendResponseHeaders(200, 0);
-			try (OutputStreamWriter out = new OutputStreamWriter(exchange.getResponseBody()))
-			{
-				RuneLiteAPI.GSON.toJson(items, out);
-			}
-		};
-	}
+            JsonObject object = new JsonObject();
+            object.addProperty("stat", skill.getName());
+            object.addProperty("level", client.getRealSkillLevel(skill));
+            object.addProperty("boostedLevel", client.getBoostedSkillLevel(skill));
+            object.addProperty("xp", client.getSkillExperience(skill));
+            skills.add(object);
+        }
 
-	private <T> T invokeAndWait(Callable<T> r)
-	{
-		try
-		{
-			AtomicReference<T> ref = new AtomicReference<>();
-			Semaphore semaphore = new Semaphore(0);
-			clientThread.invokeLater(() -> {
-				try
-				{
+        exchange.sendResponseHeaders(200, 0);
+        try (OutputStreamWriter out = new OutputStreamWriter(exchange.getResponseBody()))
+        {
+            RuneLiteAPI.GSON.toJson(skills, out);
+        }
+    }
 
-					ref.set(r.call());
-				}
-				catch (Exception e)
-				{
-					throw new RuntimeException(e);
-				}
-				finally
-				{
-					semaphore.release();
-				}
-			});
-			semaphore.acquire();
-			return ref.get();
-		}
-		catch (Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
+    private HttpHandler handlerForInv(InventoryID inventoryID)
+    {
+        return exchange -> {
+            Item[] items = invokeAndWait(() -> {
+                ItemContainer itemContainer = client.getItemContainer(inventoryID);
+                if (itemContainer != null) return itemContainer.getItems();
+                return null;
+            });
+
+            if (items == null)
+            {
+                exchange.sendResponseHeaders(204, 0);
+                return;
+            }
+
+            exchange.sendResponseHeaders(200, 0);
+            try (OutputStreamWriter out = new OutputStreamWriter(exchange.getResponseBody()))
+            {
+                RuneLiteAPI.GSON.toJson(items, out);
+            }
+        };
+    }
+
+    private <T> T invokeAndWait(Callable<T> r)
+    {
+        try
+        {
+            AtomicReference<T> ref = new AtomicReference<>();
+            Semaphore semaphore = new Semaphore(0);
+            clientThread.invokeLater(() -> {
+                try
+                {
+                    ref.set(r.call());
+                }
+                catch (Exception e)
+                {
+                    throw new RuntimeException(e);
+                }
+                finally
+                {
+                    semaphore.release();
+                }
+            });
+            semaphore.acquire();
+            return ref.get();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 }
